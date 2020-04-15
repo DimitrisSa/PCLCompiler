b996a63f0a979db142217c9d210654d055a27832
gnostis <saridakisbitos@gmail.com>
2020-04-15 19:13:07 +0300

    I am an automated comment

--- 2Semantic/Semantic.hs
+++ 2Semantic/Semantic.hs
@@ -6,7 +6,7 @@ import qualified Data.Map as M
 
 type TypeMap = M.Map Id Type
 type Error = String
-type Semantics = EitherT Error (State TypeMap) ()
+type Semantics a = EitherT Error (State TypeMap) a
 
 main = do
   ast <- parser
@@ -17,19 +17,19 @@ main = do
       Left s   -> s
 
 -- process ast
-program :: Program -> Semantics
+program :: Program -> Semantics ()
 program (P _ body) = initSymbolTable >> bodySems body 
 
 -- process body
-bodySems :: Body -> Semantics
+bodySems :: Body -> Semantics ()
 bodySems (B locals block) = 
   flocals (reverse locals) >> fblock block
 
 -- process locals (vars, labels, headbod, forward)
-flocals :: [Local] -> Semantics
+flocals :: [Local] -> Semantics ()
 flocals = mapM_ flocal
 
-flocal :: Local -> Semantics
+flocal :: Local -> Semantics ()
 flocal = \case 
   LoVar vars     -> toSems vars
   LoLabel labels -> myinsert (makeLabelList labels)
@@ -41,7 +41,7 @@ parErr = "Parameter missmatch between " ++
          "forward and declaration for: "
 dupErr = "Duplicate Variable: "
 
-headBodF :: Header -> Semantics
+headBodF :: Header -> Semantics ()
 headBodF h = do
   tm <- get
   case h of
@@ -65,7 +65,7 @@ headBodF h = do
         Nothing -> put $ M.insert i (Tfunc a t) tm
         _       -> left $ dupErr ++ i
 
-forwardF :: Header -> Semantics
+forwardF :: Header -> Semantics ()
 forwardF h = do
   tm <- get
   case h of
@@ -78,7 +78,7 @@ forwardF h = do
         Just _  -> left $ dupErr ++ i
         Nothing -> put $ M.insert i (TFfunc a t) tm
 
-toSems :: Variables -> Semantics
+toSems :: Variables -> Semantics ()
 toSems = mapM_ (myinsert . makelist)
 
 makelist :: (Ids,Type) -> [(Id,Type)]
@@ -93,7 +93,7 @@ makelisthelp (in1,myt) = Prelude.map (\_ -> myt) in1
 makeLabelList :: Ids -> [(Id,Type)]
 makeLabelList in1 = Prelude.map (\x -> (x,Tlabel)) in1 
 
-myinsert :: [(Id,Type)] -> Semantics
+myinsert :: [(Id,Type)] -> Semantics ()
 myinsert ((v,t):xs) = do
   tm <- get
   case M.lookup v tm of
@@ -101,19 +101,21 @@ myinsert ((v,t):xs) = do
     Nothing -> put (M.insert v t tm) >> myinsert xs
 myinsert [] = return ()
 
-fblock :: Block -> Semantics 
+fblock :: Block -> Semantics () 
 fblock (Bl ss) = mapM_ fstatement ss
 
 -- Check that label exists in the program (not done)
 -- Check that r-value is not procedure in call (not done)
 -- Check that forward is declared afterwards (not done)
 -- fix call expr for r-values
+-- write particular argument of type mismatch
 -- string-literal
+-- totypel check out of bounds
 
 gotoErr = "Undeclared Label: "
 callErr = "Undeclared function or procedure in call: "
 
-fstatement :: Stmt -> Semantics
+fstatement :: Stmt -> Semantics ()
 fstatement = \case
   SEmpty               -> return ()
   SEqual lValue expr   -> return ()
@@ -142,21 +144,179 @@ callSem id = \case
   TFfunc as t -> \exprs -> return ()
   Tfunc  as t -> \exprs -> return ()
   TFproc as   -> \exprs -> return ()
-  Tproc  as   -> \exprs -> argsExprsSems id (makelistforward as) exprs--mapM totype exprs >>= argsExprsSems id (makelistforward as)
+  Tproc  as   -> \exprs ->
+    mapM totype exprs >>=
+    argsExprsSems id (makelistforward as)
   _           -> \_ -> left $ callSemErr ++ id
 
---totype :: Expr -> Semantics 
---totype a = Tint
+totype :: Expr -> Semantics Type
+totype = \case
+  L lval -> totypel lval
+  R rval -> totyper rval
+  EEmpty -> left $ "Empty Expr"
+
+varErr = "Undeclared variable: "
+retErr = "'return' in function argument"
+indErr = "index not integer"
+arrErr = "indexing something that is not an array"
+pointErr = "dereferencing non-pointer"
+
+totypel :: LValue -> Semantics Type
+totypel = \case
+  LId id                 -> do
+    tm <- get
+    case M.lookup id tm of
+      Just t  -> return t
+      Nothing -> left $ varErr ++ id
+  LResult                -> left retErr
+  LString string         -> right $ ArrayT NoSize Tchar
+  LValueExpr lValue expr -> totype expr >>= \case 
+    Tint -> totypel lValue >>= \case
+      ArrayT _ t -> right t
+      _          -> left arrErr
+    _    -> left indErr
+  LExpr expr             -> totype expr >>= \case
+    PointerT t -> right t
+    _          -> left pointErr
+  LParen lValue          -> totypel lValue
+
+totyper :: RValue -> Semantics Type
+totyper = \case
+  RInt _              -> right Tint
+  RTrue               -> right Tbool
+  RFalse              -> right Tbool
+  RReal _             -> right Treal
+  RChar _             -> right Tchar
+  RParen rValue       -> totyper rValue
+  RNil                -> undefined
+  RCall (CId id expr) -> undefined
+  RPapaki  lValue     -> totypel lValue >>= right . PointerT
+  RNot     expr       -> totype expr >>= \case
+    Tbool -> right Tbool
+    _     -> left "non-boolean expression after not" 
+  RPos     expr       -> totype expr >>= \case
+    Tint  -> right Tint
+    Treal -> right Treal
+    _     -> left "non-number expression after '+'"
+  RNeg     expr       -> totype expr >>= \case
+    Tint  -> right Tint
+    Treal -> right Treal
+    _     -> left "non-number expression after '-'"
+  RPlus    exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tint
+      Treal -> right Treal
+      _     -> left "non-number expression after '+'"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Treal
+      Tint  -> right Treal
+      _     -> left "non-number expression after '+'"
+    _     -> left "non-number expression before '+'"
+  RMul     exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tint
+      Treal -> right Treal
+      _     -> left "non-number expression after '*'"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Treal
+      Tint  -> right Treal
+      _     -> left "non-number expression after '*'"
+    _     -> left "non-number expression before '*'"
+  RMinus   exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tint
+      Treal -> right Treal
+      _     -> left "non-number expression after '-'"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Treal
+      Tint  -> right Treal
+      _     -> left "non-number expression after '-'"
+    _     -> left "non-number expression before '-'"
+  RRealDiv exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Treal
+      Treal -> right Treal
+      _     -> left "non-number expression after '/'"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Treal
+      Tint  -> right Treal
+      _     -> left "non-number expression after '/'"
+    _     -> left "non-number expression before '/'"
+  RDiv     exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tint
+      _     -> left "non-integer expression after 'div'"
+    _     -> left "non-integer expression before 'div'"
+  RMod     exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tint
+      _     -> left "non-integer expression after 'mod'"
+    _     -> left "non-integer expression before 'mod'"
+  ROr      exp1 exp2  -> totype exp1 >>= \case
+    Tbool  -> totype exp2 >>= \case
+      Tbool  -> right Tbool
+      _     -> left "non-boolean expression after 'or'"
+    _     -> left "non-boolean expression before 'or'"
+  RAnd     exp1 exp2  -> totype exp1 >>= \case
+    Tbool  -> totype exp2 >>= \case
+      Tbool  -> right Tbool
+      _     -> left "non-boolean expression after 'and'"
+    _     -> left "non-boolean expression before 'and'"
+  REq      exp1 exp2  -> undefined
+  RDiff    exp1 exp2  -> undefined
+  RLess    exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tbool
+      Treal -> right Tbool
+      _     -> left "non-number expression after '<'"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Tbool
+      Tint  -> right Tbool
+      _     -> left "non-number expression after '<'"
+    _     -> left "non-number expression before '<'"
+  RGreater exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tbool
+      Treal -> right Tbool
+      _     -> left "non-number expression after '>'"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Tbool
+      Tint  -> right Tbool
+      _     -> left "non-number expression after '>'"
+    _     -> left "non-number expression before '>'"
+  RGreq    exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tbool
+      Treal -> right Tbool
+      _     -> left "non-number expression after '>='"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Tbool
+      Tint  -> right Tbool
+      _     -> left "non-number expression after '>='"
+    _     -> left "non-number expression before '>='"
+  RSmeq    exp1 exp2  -> totype exp1 >>= \case
+    Tint  -> totype exp2 >>= \case
+      Tint  -> right Tbool
+      Treal -> right Tbool
+      _     -> left "non-number expression after '<='"
+    Treal -> totype exp2 >>= \case
+      Treal -> right Tbool
+      Tint  -> right Tbool
+      _     -> left "non-number expression after '<='"
+    _     -> left "non-number expression before '<='"
 
 argsExprsErr = "Wrong number of args for: "
-argsExprsSems :: Id -> [Type] -> Exprs -> Semantics
-argsExprsSems id (t:ts) (expr:exprs) = 
-  argsExprsSems id ts exprs 
+typeExprsErr = "Type mismatch of args for: "
+argsExprsSems :: Id -> [Type] -> [Type] -> Semantics ()
+argsExprsSems id (t1:t1s) (t2:t2s) | t1 == t2 = 
+  argsExprsSems id t1s t2s 
+                         | otherwise = left $ typeExprsErr ++id
+                                                
 argsExprsSems _ [] [] = return ()
 argsExprsSems id _ _ = left $ argsExprsErr ++ id
 
 --initialize Symbol Table with predefined procedures
-initSymbolTable :: Semantics
+initSymbolTable :: Semantics ()
 initSymbolTable = do
   helpprocs "writeInteger" [(["number"],Tint)]
   helpprocs "writeBoolean" [(["cow"],Tbool)]
@@ -185,14 +345,14 @@ initSymbolTable = do
   return ()
 
 --helper function to insert the predefined procedures to the symbol table
-helpprocs :: String->Args->Semantics
+helpprocs :: String->Args->Semantics ()
 helpprocs name myArgs = do
   tm <- get
   put $ M.insert name (Tproc myArgs) tm
   return ()
 
 --helper function to insert the predefined functions to the symbol table
-helpfunc :: String->Args->Type->Semantics
+helpfunc :: String->Args->Type->Semantics ()
 helpfunc name myArgs myType = do
   tm <- get
   put $ M.insert name (Tfunc myArgs myType) tm
